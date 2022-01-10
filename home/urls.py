940b1f47224b4f73abe673367051b94729626219
Boyuan Liu <boyuanliu6@yahoo.com>
2022-01-09 19:59:48 -0800

    added trending query

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("search/", views.search, name="search"),
     path("tv/<int:tv_id>/", views.view_tv_detail, name="tvdetail"),
     path("movie/<int:movie_id>/", views.view_movie_detail, name="moviedetail"),
+    path("api/trendings/", views.view_trendings_results, name="trendings"),
 ]
--- home/views.py
+++ home/views.py
@@ -2,6 +2,7 @@ from django.http.response import JsonResponse
 from django.shortcuts import render
 import requests
 from django.http import HttpResponse
+import json
 
 # Create your views here.
 
@@ -14,25 +15,12 @@ def search(request):
     print(query)
 
     # If the query is not empty
-    results = []
     if query:
 
         # Get the results from the API
 
         data = requests.get(f"https://api.themoviedb.org/3/search/{request.GET.get('type')}?api_key={TMDB_API_KEY}&language=en-US&page=1&include_adult=false&query={query}")
         print(data.json())
-
-        # temp = []
-        # for m in data.json()["results"]:
-
-        #     if len(temp) < 3:
-        #         temp.append({"name": m["name"], "poster": m["poster_path"], "overview": m["overview"]})
-        #     else:
-        #         results.append(temp)
-        #         temp.append({"name": m["name"], "poster": m["poster_path"], "overview": m["overview"]})
-
-        # results.append(temp) if len(temp) > 0 else None
-
 
     else:
         return HttpResponse("Please enter a search query")
@@ -48,12 +36,26 @@ def index(request):
 
 def view_tv_detail(request, tv_id):
     data = requests.get(f"https://api.themoviedb.org/3/tv/{tv_id}?api_key={TMDB_API_KEY}&language=en-US")
+    recommendations = requests.get(f"https://api.themoviedb.org/3/tv/{tv_id}/recommendations?api_key={TMDB_API_KEY}&language=en-US")
     return render(request, "home/tv_detail.html", {
-        "data": data.json()
+        "data": data.json(),
+        "recommendations": recommendations.json(),
+        "type": "tv",
     })
 
 def view_movie_detail(request, movie_id):
     data = requests.get(f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={TMDB_API_KEY}&language=en-US")
+    recommendations = requests.get(f"https://api.themoviedb.org/3/movie/{movie_id}/recommendations?api_key={TMDB_API_KEY}&language=en-US")
     return render(request, "home/movie_detail.html", {
-        "data": data.json()
+        "data": data.json(),
+        "recommendations": recommendations.json(),
+        "tv": "movie",
     })
+
+
+def view_trendings_results(request):
+    type = request.GET.get("media_type")
+    time_window = request.GET.get("time_window")
+
+    trendings = requests.get(f"https://api.themoviedb.org/3/trending/{type}/{time_window}?api_key={TMDB_API_KEY}&language=en-US")
+    return JsonResponse(trendings.json())
